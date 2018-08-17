f6101626bcb8daa8c72ac805019321ecd205a6b9
hanfamilym1 <michael@hanfamily.com>
2018-08-16 20:18:20 -0600

    worked

--- findDigit.js
+++ findDigit.js
@@ -1,4 +1,4 @@
-//https://www.codewars.com/kata/577b9960df78c19bca00007e
+// https://www.codewars.com/kata/577b9960df78c19bca00007e
 // Complete the function that takes two numbers as input,
 // num and nth and return the nth digit of num (counting from right to left).
 
